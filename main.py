bca5bec32f8ae531d62b2e07a85b253d547182c8
Hash-Brown15 <JackHeppell1@gmail.com>
2025-10-22 21:02:08 +0100

    Add files via upload

--- main.py
+++ main.py
@@ -1,29 +1,44 @@
 from tkinter import*
 import tkinter.font as font
+import random
 
-my_window = Tk()
-my_window.geometry("500x500")
+game_window = Tk()
+game_window.title("Rock Paper Scissors")
 
-decription = Label(my_window, text="Rock, paper, scissors", font=font.Font(size=20,fg="grey"))
-decription.pack()
+app_font = font.Font(size=12)
 
-fame = Frame
-frame.pack(pady=20)
+game_title = Label(game_window, text="Rock, paper, scissors", font=font.Font(size=20),fg="grey")
+game_title.pack()
 
-title = Label(frame, text="Rock Paper Scissors", font=font.Font(size=15))
-title.grid(row=0, column=0)
+winner_label = Label(game_window, text="", fg = "green", font= font.Font (size = 8))
+winner_label.pack()
 
-options = Label(frame, text="Your Options:" ,font=font.Font(size=15))
-options.grid(row=1, column=0)
+input_frame = Frame(game_window)
+input_frame.pack(pady=20)
 
-score = Label(frame, text="Score:", font=font.Font(size=15))
-score.grid(row=2, column=0)
+rock_btn = Button(input_frame, text="Rock", width=15, bd=0, bg="pink", pady=5)
+rock_btn.grid(row=1, column=1, padx=8, pady=5)
 
-rock = Button(frame, text="Rock", font=font.Font(size=15), bg="red", fg="white", padx=20, pady=10)
-rock.grid(row=1, column=1, columnspan = 2, padx=10)
+paper_btn = Button(input_frame, text="Paper", width=15, bd=0, bg="blue", pady=5)
+paper_btn.grid(row=1, column=2, padx=8, pady=5)
 
-paper = Button(frame, text="Paper", font=font.Font(size=15), bg="blue", fg="white", padx=20, pady=10)
-paper.grid(row=1, column=3, columnspan = 2, padx=10)
+scissors_btn = Button(input_frame, text="Scissors", width=15, bd=0, bg="green", pady=5)
+scissors_btn.grid(row=1, column=3, padx=8, pady=5)
 
-scissors = Button(frame, text="Scissors", font=font.Font(size=15), bg="green", fg="white", padx=20, pady=10)
-scissors.grid(row=1, column=5, columnspan = 2, padx=10)
+score_label = Label(input_frame, text="Score :", font = app_font, fg = "grey")
+score_label.grid(row=2, column=0)
+
+player_choice_label = Label(input_frame, text="Your Selected :---", font=app_font)
+player_choice_label.grid(row=3, column=1, pady = 5)
+
+player_score_label = Label(input_frame, text="Your Score :---", font=app_font)
+player_score_label.grid(row=3, column=2, pady=5)
+
+computer_choice_label = Label(input_frame, text="Computer Selected :---", font=app_font, fg = "black")
+computer_choice_label.grid(row=4, column=1, pady=5)
+
+computer_score_label = Label(input_frame, text="Computer Score :---", font=app_font, fg = "black")
+computer_score_label.grid(row=4, column=2, padx=(10,0), pady=5)
+
+game_window.geometry("700x300")
+game_window.mainloop()
